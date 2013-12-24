573ba654b9927a3f91e9308f38427d9c99b7f5c9
Aleksandar Diklic <rastasheep3@gmail.com>
2013-12-24 11:33:46 +0100

    First working version of docs

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+gem "middleman"
+
+gem "redcarpet"
+gem "middleman-syntax"
+
+gem "middleman-s3_sync"
+
+gem "nokogiri"
+
+gem "debugger"
---
+++ config.rb
@@ -0,0 +1,104 @@
+###
+# Middleman settings
+###
+
+# Time.zone = "UTC"
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+activate :syntax #https://github.com/middleman/middleman-syntax
+
+#Activate sync extension
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                = 'semaphore-docs' # The name of the S3 bucket you are targetting. This is globally unique.
+  s3_sync.aws_access_key_id     = ''
+  s3_sync.aws_secret_access_key = ''
+  s3_sync.delete                = false # We delete stray files by default.
+  s3_sync.after_build           = false # We chain after the build step by default. This may not be your desired behavior...
+end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+set :css_dir, 'assets/css'
+
+set :js_dir, 'assets/js'
+
+set :images_dir, 'assets/images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
+
+# Used for generating absolute URLs:
+helpers do
+
+  def host_with_port
+    [host, optional_port].compact.join(':')
+  end
+
+  def optional_port
+    port unless port.to_i == 80
+  end
+
+  def image_url(source)
+    protocol + host_with_port + image_path(source)
+  end
+
+end
+
+configure :development do
+  if defined? Middleman::PreviewServer # false when doing s3_sync, no idea
+    set :protocol, "http://"
+    set :host, Middleman::PreviewServer.host
+    set :port, Middleman::PreviewServer.port
+  else
+    set :protocol, "https://"
+    set :host, "semaphoreapp.com"
+    set :port, 80
+  end
+end
+
+configure :build do
+  set :protocol, "https://"
+  set :host, "semaphoreapp.com"
+  set :port, 80
+end
