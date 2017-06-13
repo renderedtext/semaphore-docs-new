ca9c1e9c438560593559a15365b9f2fba52208af
Jovan Ivanović <jivanovic@renderedtext.com>
2017-06-13 11:39:02 +0200

    Update sidebar JS

--- source/docs/assets/js/sidebar.js
+++ source/docs/assets/js/sidebar.js
@@ -1,17 +1,28 @@
-$(document).ready(function() {
-  var title = $(".article-title").text();
+const current_category_class = "link-reset db black-80 pv1 pl4"
+const current_article_class  = "link-reset db normal green b pv1"
 
-  $(".article-key").each(function() {
-    if ($(this).text() === title) {
-      $(this).next().attr('class', 'link-reset db normal green b pv1');
-      $(this).parent().addClass('current-category').slideToggle();
-    };
-  });
+$(document).ready(function() {
+  const title = $(".article-title").text();
+
+  if (title === "") {
+    $(".docs-nav-home a").attr('class', current_category_class)
+  } else {
+    $(".article-key").each(function() {
+      if ($(this).text() === title) {
+        $(this).next().attr('class', current_article_class);
+
+        const parent = $(this).parent();
+
+        parent.prev().attr('class', current_category_class);
+        parent.addClass('current-category').slideToggle();
+      };
+    });
+  }
 
   $(".category-key").click(function(event) {
     event.preventDefault();
 
-    var key = $(this).text();
+    const key = $(this).text();
 
     $(".category-div").not(".current-category").each(function() {
       if ($(this).prev().text() !== key) {
