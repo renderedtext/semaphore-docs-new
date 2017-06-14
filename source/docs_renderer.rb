6a139e0a4a9af6336aefa1ce2912b0677945f5d0
Jovan Ivanović <jivanovic@renderedtext.com>
2017-06-14 12:01:16 +0200

    Escape HTML in custom rendereds

--- source/docs_renderer.rb
+++ source/docs_renderer.rb
@@ -1,6 +1,8 @@
+require "cgi"
+
 class DocsRenderer < Redcarpet::Render::HTML
   def block_quote(quote)
-    %(<blockquote cite="http://developer.mozilla.org" class="mh0 ph4 pv1 f5 lh-loose bl b--green bw2 black-70 bg-near-white">#{quote}</blockquote>)
+    %(<blockquote cite="http://developer.mozilla.org" class="mh0 ph4 pv1 f5 lh-loose bl b--green bw2 black-70 bg-near-white">#{escape_html(quote)}</blockquote>)
   end
 
   def table(header, body)
@@ -8,10 +10,16 @@ class DocsRenderer < Redcarpet::Render::HTML
   end
 
   def codespan(code)
-    %(<span class="f7 code bg-washed-grayblue black-50blue br2 pa1">#{code}</span>)
+    %(<span class="f7 code bg-washed-grayblue black-50blue br2 pa1">#{escape_html(code)}</span>)
   end
 
   def block_code(code, language)
-    %(<pre class="pre code f7 mv4"><code class="ba b--black-20">#{code}</code></pre>)
+    %(<pre class="pre code f7 mv4"><code class="ba b--black-20">#{escape_html(code)}</code></pre>)
+  end
+
+  private
+
+  def escape_html(string)
+    CGI.escapeHTML(string)
   end
 end
