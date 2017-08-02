1d40a5ebf6f61aa82d5f9fa1c80c4078e7f55291
Ervin Barta <ervin@renderedtext.com>
2017-08-02 12:38:18 +0200

    [WIP] Add link checker script

---
+++ script/link-checker.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+#
+# Validates links in Middleman generated pages using http-proover
+# -
+# The latest version of http-proover requires major gem updates in the repository
+# so the script is currently self contained -- the gem is not part of the main
+# Gemfile and it has to be installed manually
+# -
+# https://github.com/gjtorikian/html-proofer
+
+TARGET_DIRECTORY = "#{File.expand_path File.dirname(__FILE__)}/../build"
+
+# Ignored string in paths eg. www.semaphoreci.com/docs/pricing
+## These links appear mostly in the header/footer -- these paths need to addressed
+ignored_paths = %w(
+/pricing
+/product
+/privacy
+/community
+/contact
+/tos
+/security
+/press
+/about
+)
+
+# Ignored regular expression in paths eg. www.semaphoreci.com/docs/users?sign_in
+ignored_paths.concat [ /^\/$/, /renderedtext/, /blog/, /users/ ]
+
+options = {
+  :extension => ".html",
+  :allow_hash_href => true,
+  :alt_ignore => [/.*/],
+  :url_ignore => ignored_paths
+}
+
+puts ">> Installing html-proofer"
+system("gem install html-proofer --no-ri --no-rdoc")
+require "html-proofer"
+
+puts ">> Building pages with Middleman"
+system("cd #{File.expand_path Dir.pwd}; bundle exec middleman build")
+
+HTMLProofer.check_directory(TARGET_DIRECTORY, options).run
